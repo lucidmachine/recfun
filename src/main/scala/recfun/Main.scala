a889e968472ba0d20fe1e70543121529612e141b
lucidmachine <lucid.machine@gmail.com>
2017-09-05 22:06:01 -0400

    Implement pascal().

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -11,26 +11,24 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-
-  /**
     * Exercise 1 - Pascal's Triangle
     *
     * Returns the number at the given coordinates in Pascal's triangle.
-    * @param c Column number, left to right.
-    * @param r Row number, top to bottom.
+    * @param c Column number, 0 to c, left to right.
+    * @param r Row number, 0 to r, top to bottom.
     * @return The number in the given position.
     */
-    def pascal(c: Int, r: Int): Int = ???
-  
+  def pascal(c: Int, r: Int): Int =
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r) * (r - c + 1) / c
+
   /**
    * Exercise 2
    */
-//    def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = false
   
   /**
    * Exercise 3
    */
-//    def countChange(money: Int, coins: List[Int]): Int = ???
-  }
+  def countChange(money: Int, coins: List[Int]): Int = 0
+}
