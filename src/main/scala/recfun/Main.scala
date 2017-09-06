4aec2539874dbf55bfb4c1b06f42e02ba912de2d
lucidmachine <lucid.machine@gmail.com>
2017-09-06 06:21:00 -0400

    Implement balance().

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -19,8 +19,8 @@ object Main {
     * @return The number in the given position.
     */
   def pascal(c: Int, r: Int): Int =
-    if (c == 0 || c == r) 1
-    else pascal(c - 1, r) * (r - c + 1) / c
+    if (c == 0 || c == r) 1 // edge
+    else pascal(c - 1, r) * (r - c + 1) / c // binomial
 
   /**
     * Exercise 2 - Parentheses Balancing
@@ -29,7 +29,19 @@ object Main {
     * @param chars The list of characters to test for balance.
     * @return Whether the given list of characters has balanced parentheses.
     */
-  def balance(chars: List[Char]): Boolean = false
+  def balance(chars: List[Char]): Boolean = {
+    def isBalanced(chars: List[Char], currBalance: Int): Boolean =
+      if (currBalance < 0) false // If the balance becomes negative there are too many )
+      else
+        if (chars.isEmpty) currBalance == 0 // check whether balance was maintained at end of list
+        else chars.head match {
+          case '(' => isBalanced(chars.tail, currBalance + 1)
+          case ')' => isBalanced(chars.tail, currBalance - 1)
+          case _ => isBalanced(chars.tail, currBalance)
+        }
+
+    isBalanced(chars, 0)
+  }
   
   /**
    * Exercise 3
